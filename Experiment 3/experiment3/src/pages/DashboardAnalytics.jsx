039c0308f2ab4318d5740311a9fd4c97aff9ac42
Madhur <141059674+Avantigithub01@users.noreply.github.com>
2026-03-01 22:06:28 +0530

    Delete Experiment 3 directory

--- Experiment 3/experiment3/src/pages/DashboardAnalytics.jsx
+++
@@ -1,6 +0,0 @@
-const DashboardAnalytics=()=>{
-    return (
-        <p>This is the Dashboard Analytics</p>
-    )
-}
-export default DashboardAnalytics;
--- Experiment 3/experiment3/src/pages/DashboardSummary.jsx
+++
@@ -1,6 +0,0 @@
-const DashboardSummary=()=>{
-    return(
-        <p>This is the Dashboard summary</p>
-    )
-}
-export default DashboardSummary;
